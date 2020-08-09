b7d9acaa0570700d61edcb57b94913f00e3ed231
Mandy <chenjiafeng16@126.com>
2020-08-09 23:22:02 +0800

    sleep

---
+++ array/index.js
@@ -0,0 +1,28 @@
+// console.log("1.Array.of()———将参数中的所有值作为元素形成数组");
+// console.log(Array.of(1,2,3,4));
+// console.log(Array.of(1,"2",false));
+// console.log(Array.of(1,"2",false));
+// console.log(Array.of(3));
+// console.log(Array.of(3).length);
+// console.log(Array.of());
+// console.log(Array.of(null));
+// console.log(Array.of(undefined));
+// console.log(Array(3));
+// console.log(Array(3).length);
+
+
+// let array1=[1,2,3,4];
+// let array2=[1,2,3,4]
+// console.log(array1==array2);
+// console.log(array1===array2);
+
+// console.log("Array.from()——将类数组对象或可迭代对象转化为数组。")
+// console.log(Array.from([1,2]));
+// console.log(Array.from([1,,2]));
+// console.log(Array.from([1]));
+// console.log(Array.from([null]));
+// console.log(Array.from(8));
+// console.log(Array.from(null));
+// console.log(Array.from(undefined));
+// console.log(Array.from());
+
