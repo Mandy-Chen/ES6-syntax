cc36dd876cedbc9e93bf1c942fa5562ad5d5aa6e
Mandy <chenjiafeng16@126.com>
2020-08-16 03:02:35 +0800

    [array_lodash][mandy] feature : difference()

--- array_lodash/index.js
+++ array_lodash/index.js
@@ -8,6 +8,11 @@ var _ = require('lodash');
 // console.log(_.compact(array));  //[ 1, 2, 3, 4 ]
 // console.log(array);  //[ 1, 2, NaN, null, undefined, 0, false, 3, 4 ]
 
+// let array = [1, 2, 3];
+// console.log(_.concat(array, 4, [5], [[6]])); //[ 1, 2, 3, 4, 5, [ 6 ] ]
+// console.log(array); //[ 1, 2, 3 ]
+
 let array = [1, 2, 3];
-console.log(_.concat(array, 4, [5], [[6]])); //[ 1, 2, 3, 4, 5, [ 6 ] ]
+console.log(_.difference(array, [1])); //[ 2, 3 ]
+console.log(_.difference(array, [1, 2, 3, 4])); //[]
 console.log(array); //[ 1, 2, 3 ]
