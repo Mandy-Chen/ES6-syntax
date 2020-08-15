2dbb946d1a90cb7da1e78a1abc7fb7a8e9fcd2b3
Mandy <chenjiafeng16@126.com>
2020-08-16 03:00:20 +0800

    [array_lodash][mandy] feature : concat()

--- array_lodash/index.js
+++ array_lodash/index.js
@@ -4,6 +4,10 @@ var _ = require('lodash');
 // console.log(_.chunk(array1,3));  //[ [ 1, 2, 3 ], [ 4 ] ]
 // console.log(array1);  //[ 1, 2, 3, 4 ]
 
-let array = [1, 2, NaN, null, undefined, 0, false, 3, 4];
-console.log(_.compact(array));  //[ 1, 2, 3, 4 ]
-console.log(array);  //[ 1, 2, NaN, null, undefined, 0, false, 3, 4 ]
+// let array = [1, 2, NaN, null, undefined, 0, false, 3, 4];
+// console.log(_.compact(array));  //[ 1, 2, 3, 4 ]
+// console.log(array);  //[ 1, 2, NaN, null, undefined, 0, false, 3, 4 ]
+
+let array = [1, 2, 3];
+console.log(_.concat(array, 4, [5], [[6]])); //[ 1, 2, 3, 4, 5, [ 6 ] ]
+console.log(array); //[ 1, 2, 3 ]
