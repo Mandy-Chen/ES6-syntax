3e48e985aadd1023a923c08de510b06d5881e536
Mandy <chenjiafeng16@126.com>
2020-08-16 02:56:27 +0800

    [array_lodash][mandy] feature : compact()

--- array_lodash/index.js
+++ array_lodash/index.js
@@ -1,6 +1,9 @@
 var _ = require('lodash');
-var array = require('lodash/array');
-let array1=[1,2,3,4];
-console.log(_.chunk(array1,2)); //[ [ 1, 2 ], [ 3, 4 ] ]
-console.log(_.chunk(array1,3));  //[ [ 1, 2, 3 ], [ 4 ] ]
-console.log(array1);  //[ 1, 2, 3, 4 ]
+// let array1=[1,2,3,4];
+// console.log(_.chunk(array1,2)); //[ [ 1, 2 ], [ 3, 4 ] ]
+// console.log(_.chunk(array1,3));  //[ [ 1, 2, 3 ], [ 4 ] ]
+// console.log(array1);  //[ 1, 2, 3, 4 ]
+
+let array = [1, 2, NaN, null, undefined, 0, false, 3, 4];
+console.log(_.compact(array));  //[ 1, 2, 3, 4 ]
+console.log(array);  //[ 1, 2, NaN, null, undefined, 0, false, 3, 4 ]
