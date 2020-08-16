cbb872e91cda2dd1ca693804d6ee3dd3b0a86ded
Mandy <chenjiafeng16@126.com>
2020-08-16 15:43:54 +0800

    [array_lodash][mandy] feature : findIndex()

--- array_lodash/index.js
+++ array_lodash/index.js
@@ -53,10 +53,15 @@ var _ = require('lodash');
 // console.log(_.dropWhile([6,5,4,3,2,1],item=>item>3)); //[ 3, 2, 1 ]
 // console.log(_.dropWhile([3,2,6,4,5,1],item=>item>3)); //[ 3, 2, 6, 4, 5, 1 ]
 
-let array = [1, 2, 3, 4, 5];
-console.log(_.fill(array, 9, 1, 3)); //[ 1, 9, 9, 4, 5 ]
-console.log(array); //[ 1, 9, 9, 4, 5 ]
-console.log(_.fill(array, 666)); //[ 666, 666, 666, 666, 666 ]
-console.log(array); //[ 666, 666, 666, 666, 666 ]
-console.log(_.fill(array, 777, 3)); //[ 666, 666, 666, 777, 777 ]
-console.log(array); //[ 666, 666, 666, 777, 777 ]
+// let array = [1, 2, 3, 4, 5];
+// console.log(_.fill(array, 9, 1, 3)); //[ 1, 9, 9, 4, 5 ]
+// console.log(array); //[ 1, 9, 9, 4, 5 ]
+// console.log(_.fill(array, 666)); //[ 666, 666, 666, 666, 666 ]
+// console.log(array); //[ 666, 666, 666, 666, 666 ]
+// console.log(_.fill(array, 777, 3)); //[ 666, 666, 666, 777, 777 ]
+// console.log(array); //[ 666, 666, 666, 777, 777 ]
+
+let array = [1, 2, 3, 4, 5, 3, 2, 5];
+console.log(_.findIndex(array, item => item > 3)); //3
+console.log(_.findIndex(array, item => item > 3, 4)); //4
+console.log(array); //[1,2,3,4,5,3,2,5]
