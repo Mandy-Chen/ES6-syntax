13ff7e3a5f0196dfbee8218d5998ee96fef88902
MandyChen <497133212@qq.com>
2020-08-20 17:58:12 +0800

    [oject][mandy] feature : Object.create()

--- test/test.js
+++ test/test.js
@@ -14,41 +14,56 @@ describe('Object', function () {
   //     array2.constructor.should.equal(Array);
   //   });
   // });
-  describe('Object.assign()', function () {
-    it('source object is copied to target object.', function () {
-      let source = { a: 1, b: 2 };
-      let target = { c: 3, d: 4 };
-      let result = Object.assign(source, target);
-      console.log(result); //{ a: 1, b: 2, c: 3, d: 4 }
-      result.should.not.equal({ a: 1, b: 2, c: 3, d: 4 });
-      result.should.equal(source);
-      let target1 = { b: 8888, d: 4 };
-      let result1 = Object.assign(source, target1);
-      console.log(result1); //{ a: 1, b: 8888, c: 3, d: 4 }
-      result1.should.equal(source);
-    })
-    it('Inheritance properties and non-enumerated properties cannot be copied.', function () {
-      let object = Object.create(
-        { age: 18 }, //age是个继承属性
-        {
-          name: {  //name是不可枚举属性
-            value: "mandy"
-          },
-          salary: { //salary是自身可枚举属性
-            value: "99999",
-            enumerable: true,
-          }
-        });
-      let result = Object.assign({}, object);
-      console.log(result); //{ salary: '99999' }
-      result.should.not.equal(object);
-    });
-    it('original type is wrapped as an object.', function () {
-      let str = "abc";
-      let boo = true;
-      let num = 100;
-      let result = Object.assign({}, null, boo, undefined, str, num);
-      console.log(result); //{ '0': 'a', '1': 'b', '2': 'c' }
+  // describe('Object.assign()', function () {
+  //   it('source object is copied to target object.', function () {
+  //     let source = { a: 1, b: 2 };
+  //     let target = { c: 3, d: 4 };
+  //     let result = Object.assign(source, target);
+  //     console.log(result); //{ a: 1, b: 2, c: 3, d: 4 }
+  //     result.should.not.equal({ a: 1, b: 2, c: 3, d: 4 });
+  //     result.should.equal(source);
+  //     let target1 = { b: 8888, d: 4 };
+  //     let result1 = Object.assign(source, target1);
+  //     console.log(result1); //{ a: 1, b: 8888, c: 3, d: 4 }
+  //     result1.should.equal(source);
+  //   })
+  //   it('Inheritance properties and non-enumerated properties cannot be copied.', function () {
+  //     let object = Object.create(
+  //       { age: 18 }, //age是个继承属性
+  //       {
+  //         name: {  //name是不可枚举属性
+  //           value: "mandy"
+  //         },
+  //         salary: { //salary是自身可枚举属性
+  //           value: "99999",
+  //           enumerable: true,
+  //         }
+  //       });
+  //     let result = Object.assign({}, object);
+  //     console.log(result); //{ salary: '99999' }
+  //     result.should.not.equal(object);
+  //   });
+  //   it('original type is wrapped as an object.', function () {
+  //     let str = "abc";
+  //     let boo = true;
+  //     let num = 100;
+  //     let result = Object.assign({}, null, boo, undefined, str, num);
+  //     console.log(result); //{ '0': 'a', '1': 'b', '2': 'c' }
+  //   });
+  // })
+  describe('Object.create()', function () {
+    it('new object.', function () {
+      const person = {
+        isGirl: false,
+        info: function () {
+          return 'My name is ' + this.name + ' ,am I a girl : ' + this.isGirl;
+        }
+      }
+      const result = Object.create(person);
+      result.name = "mandy";
+      result.isGirl = true;
+      console.log(result);  //{ name: 'mandy', isGirl: true }
+      result.info().should.equal("My name is mandy ,am I a girl : true");
     });
   })
 
