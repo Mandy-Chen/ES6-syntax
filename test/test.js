5bf5f9da1d545d553e9facf5330d3a6bb7001eee
MandyChen <497133212@qq.com>
2020-08-20 16:44:18 +0800

    [object][mandy chen] feature : Object.assign() :source object is copied to target object.

--- test/test.js
+++ test/test.js
@@ -14,21 +14,34 @@ var should = require('should');
 //   });
 // });
 
-describe('Object',function(){
-  describe('Object.prototype.constructor',function(){
-    it('All objects inherit  constructor property from its prototype.',function(){
-      let object1={};
-      object1.constructor.should.equal(Object);
-      let object2=new Object();
-      object2.constructor.should.equal(Object);
-      let array1=[];
-      array1.constructor.should.equal(Array);
-      let array2=new Array();
-      array2.constructor.should.equal(Array);
-    });
+describe('Object', function () {
+  // describe('Object.prototype.constructor', function () {
+  //   it('All objects inherit  constructor property from its prototype.', function () {
+  //     let object1 = {};
+  //     object1.constructor.should.equal(Object);
+  //     let object2 = new Object();
+  //     object2.constructor.should.equal(Object);
+  //     let array1 = [];
+  //     array1.constructor.should.equal(Array);
+  //     let array2 = new Array();
+  //     array2.constructor.should.equal(Array);
+  //   });
+  // });
+  describe('Object.assign()', function () {
+    it('source object is copied to target object.', function () {
+      let source = { a: 1, b: 2 };
+      let target = { c: 3, d: 4 };
+      let result = Object.assign(source, target);
+      console.log(result); //{ a: 1, b: 2, c: 3, d: 4 }
+      result.should.not.equal({ a: 1, b: 2, c: 3, d: 4 });
+      result.should.equal(source);
+      let target1={b:8888,d:4};
+      let result1=Object.assign(source,target1);
+      console.log(result1); //{ a: 1, b: 8888, c: 3, d: 4 }
+      result1.should.equal(source);
+    })
+  })
 
-  });
-  
 
 });
 
