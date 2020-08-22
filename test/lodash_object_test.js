f92e0122600a518e7391829dd9724ebc2c1f1312
Mandy <chenjiafeng16@126.com>
2020-08-23 01:02:55 +0800

    [lodash_object][mandy] feature : _.assignIn()

--- test/lodash_object_test.js
+++ test/lodash_object_test.js
@@ -3,16 +3,31 @@ var _ = require('lodash');
 describe('Lodash_Object', function () {
     describe('_.assign()', function () {
         it('Assign the enumerable properties of the source object to the target object', function () {
-            let teacher = {
-                id: 1,
-                name: "zhang"
+            function Teacher() {
+                this.id = 1;
             }
-            let student = {
-                id: 2,
-                age: 18
+            Teacher.prototype.name = "zhang";
+            function Student() {
+                this.id = 2;
             }
+            Student.prototype.age = 18;
             let target = { id: 0 }
-            _.assign(target, teacher, student);
+            _.assign(target, new Teacher, new Student);
+            console.log(target); //{ id: 2 }
+        })
+    })
+    describe('_.assignIn()', function () {
+        it('Traverses and inherits the properties of the source object', function () {
+            function Teacher() {
+                this.id = 1;
+            }
+            Teacher.prototype.name = "zhang";
+            function Student() {
+                this.id = 2;
+            }
+            Student.prototype.age = 18;
+            let target = { id: 0 };
+            _.assignIn(target, new Teacher, new Student);
             console.log(target); //{ id: 2, name: 'zhang', age: 18 }
         })
     })
